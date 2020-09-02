e7256d3f75f56c272cf6daf38252fcf15a1a78e0
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-09-02 22:35:38 +0900

    ドロップダウンメニューの実装3

--- dropdown.js
+++ dropdown.js
@@ -1,7 +1,9 @@
-$(function() {
-  $(".mobile-menu__item").click(function() {
-    $(".drop-nav:not(:animated)", this).slideDown();
-  }, function() {
-    $(".drop-nav:not(:animated)", this).slideUp();
-  });
+$(".drop-nav").css("display", "none");
+
+$(".mobile-menu__item").click(function() {
+  if($(".drop-nav").css("display") == "none") {
+    $(".drop-nav:not(:animated)", this).slideDown('fast'); 
+  }else{
+    $(".drop-nav:not(:animated)", this).slideUp('fast');
+  }
 });
