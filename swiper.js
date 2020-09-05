53f09d9416de2ab7f96eda8d5ebfea6b686bd480
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-09-05 11:20:02 +0900

    swiper-3

--- swiper.js
+++ swiper.js
@@ -1,21 +1,20 @@
 var mySwiper = new Swiper ('.swiper-container', {
-  // Optional parameters
-  direction: 'vertical',
   loop: true,
-
-  // If we need pagination
+  grabCursor: true,
+  effect: 'coverflow',
+  centeredSlides: true,
+  slidesPerView: 1,
+  speed: 1000,
+  breakpoints: {
+    1024: {
+      slidesPerView: 2,
+    }
+  },
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false
+  },
   pagination: {
     el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
   },
 })
