de96f82b842800a039ee99f7bd163bdf052cccc6
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-09-05 12:04:32 +0900

    swiper-4

--- swiper.js
+++ swiper.js
@@ -5,11 +5,7 @@ var mySwiper = new Swiper ('.swiper-container', {
   centeredSlides: true,
   slidesPerView: 1,
   speed: 1000,
-  breakpoints: {
-    1024: {
-      slidesPerView: 2,
-    }
-  },
+
   autoplay: {
     delay: 4000,
     disableOnInteraction: false
